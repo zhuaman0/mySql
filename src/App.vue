2eecf1d0d1733ab532cdb158b746e457dd67edca
zhuaman0 <aitbaevaman95@gmail.com>
2024-07-30 18:19:39 +0500

    feat: basic env setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="app">
+	  <UserList />
+	</div>
+ </template>
+ 
+ <script setup>
+ import UserList from './components/UserList.vue';
+ </script>
+ 
+ <style>
+ /* Global styles */
+ #app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+ }
+ </style>
+ 
